d0fd8fbeb2c2ae804e23fb49129dcf47179881df
Daniela Marín <danie0le2@gmail.com>
2024-07-29 08:51:16 -0600

    <agregue clases y estoy haciendo el menú>

--- Laboratorio1/Class1.cs
+++ Laboratorio1/Class1.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Laboratorio1
 {
-    internal class Class1
+    public class Productos()
     {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+
+        public int Precio { get; set; }
+
+        public Productos(int iD, string nombre, int precio)
+        {
+            ID = iD;
+            Nombre = nombre;
+            Precio = precio;
+        }
+        public void MostrarDatos()
+        {
+            Console.WriteLine($"ID: {ID}, Nombre: {Nombre}, Precio: {Precio}");
+        }
     }
+
+    
+    public class AcumularPrecios()
+    {
+
+    }
+
+    public class CalcularTotal()
+    {
+
+    }
+
+    public void MenuPrincipal()
+    {
+
+    }
+    
+
+
 }
--- Laboratorio1/Program.cs
+++ Laboratorio1/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Laboratorio 1: Tienda simulada");
+﻿using Laboratorio1;
+using System;
+
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine("Laboratorio 1: Tienda simulada");
+        Console.WriteLine("");
+
+        Clientes Cliente1 = new Clientes(123, "Daniela", "4768-0200", "danie0le2@gmail.com");
+        Cliente1.MostrarDatos();
+
+        Console.WriteLine(Cliente1);
+    }
+}
+
+
